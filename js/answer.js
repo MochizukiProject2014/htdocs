7561a0bba1cb8745ed321a5e8184f91311dface0
ganmo <ganmodoki0822@yahoo.co.jp>
2015-08-18 12:07:57 +0900

    細かな修正

--- js/answer.js
+++ js/answer.js
@@ -392,7 +392,7 @@ function answer_check(num){
 			var user_code2 = parser_judge.parse(codeOfUser);
 			if(hantei_for_eval_s(user_code, 3,1) != 3){ miss_answer("条件式を見直してみよう！"); return 0;}
 			else if(hantei_for_eval_s(user_code, 2,1) != 2){ miss_answer("条件式を見直してみよう！"); return 0;}
-			else if( hantei_1(user_code2,1,"にゃんにゃん\n") != true){ miss_answer("出力される文字を確認してみよう！"); return 0;}
+			else if( hantei_1(user_code2,1,"にゃんにゃん") != true){ miss_answer("出力される文字を確認してみよう！"); return 0;}
 			else { flagArr.push(true); } 
 		break;
 		case 4121:

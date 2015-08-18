6bd18d25ca6350fdaaea31f9924fc331e97afa66
ne240152 <ne240152@senshu-u.jp>
2015-08-18 18:31:35 +0900

    422正誤判定修正

--- js/answer.js
+++ js/answer.js
@@ -430,10 +430,10 @@ function answer_check(num){
 		break;
 		case 422:
 			var user_code = parser_judge.parse(codeOfUser);
-			if(hantei_3(user_code, 1, 99, 1, "100を超えました。101です。") != true){ miss_answer("条件式を見直してみよう！"); return 0;}
-			else if( hantei_3(user_code,10,20,77,"100を超えました。107です。") != true){ miss_answer("条件式を見直してみよう！"); return 0;}
-			else if( hantei_2(user_code,99, 99, "100を超えました。198です。") != true){ miss_answer("条件式を見直してみよう！"); return 0;}
-			else if( hantei_3(user_code, 12, 34, 56, "100を超えました。102です。") != true){ miss_answer("条件式を見直してみよう！"); return 0;}
+			if(hantei_3(user_code, 1, 99, 1, "100を超えました。") != true){ miss_answer("条件式を見直してみよう！"); return 0;}
+			else if( hantei_3(user_code,10,20,77,"100を超えました。") != true){ miss_answer("条件式を見直してみよう！"); return 0;}
+			else if( hantei_2(user_code,99, 99, "100を超えました。") != true){ miss_answer("条件式を見直してみよう！"); return 0;}
+			else if( hantei_3(user_code, 12, 34, 56, "100を超えました。") != true){ miss_answer("条件式を見直してみよう！"); return 0;}
 			else { flagArr.push(true); } 
 		break;
 		case 4221:
